e955b81cfcca280840f375d15362b30572c6cc3c
Anvar <kingartaved@yandex.ru>
2022-11-30 23:49:39 +0500

    ерешли к массивам

--- Lesson2/Program.cs
+++ Lesson2/Program.cs
@@ -25,14 +25,60 @@ if(c3 > max) max = c3;
 Console.WriteLine(max);
 */
 
-//Часть 2. Используем метод (функцию)
-
-int Max (int arg1, arg2, arg3) //Это функция (МЕТОД), который исполняется для своих внутренних аргументов (арг1 и тд) и возвращает указанную переменную.
+//Часть 2 (Example 8). Используем метод (функцию)
+/*
+int Max(int arg1, int arg2, int arg3) //Это функция (МЕТОД), который исполняется для своих внутренних аргументов (арг1 и тд) и возвращает указанную переменную.
 {
-int result = arg1;
-if(arg2 > result) result = arg2;
-if(arg3 > result) result = arg3;
-return result;
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
 }
+//теперь нам нужно использовать этот метод
+int a1 = 132;
+int b1 = 1;
+int c1 = -11111;
+int a2 = 29;
+int b2 = 32;
+int c2 = 1412;
+int a3 = 12;
+int b3 = 15;
+int c3 = 26546;
 
+//int max1 = Max(a1, b1, c1);
+//int max2 = Max(a2, b2, c2);
+//int max3 = Max(a3, b3, c3);
 
+//int max = Max(max1, max2, max3);
+//А можно в строчке выше, вместо аргументов метода Max написать тоже методы Max:
+
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
+
+Console.WriteLine(max);
+*/
+
+//Часть 3 (Ex 9). Массивы
+
+int Max(int arg1, int arg2, int arg3) //Это функция (МЕТОД), который исполняется для своих внутренних аргументов (арг1 и тд) и возвращает указанную переменную.
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+//теперь нам нужно использовать этот метод
+int a1 = 132;
+int b1 = 1;
+int c1 = -11111;
+int a2 = 29;
+int b2 = 32;
+int c2 = 1412;
+int a3 = 12;
+int b3 = 15;
+int c3 = 26546;
+
+//А можно в строчке выше, вместо аргументов метода Max написать тоже методы Max:
+
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
+
+Console.WriteLine(max);
